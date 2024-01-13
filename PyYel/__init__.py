8bcfe99fc7720283836d738e1a1005061a1fcf63
Nayel Blidi <nayel.blidi@ipsa.fr>
2024-01-13 23:05:56 +0100

    Add path constants

--- PyYel/__init__.py
+++ PyYel/__init__.py
@@ -1,3 +1,16 @@
 """
 PyYel packages initializer
 """
+
+import os
+
+# PyYeL
+DIR_PYYEL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# PyYel/Data
+DIR_DATA_PATH = os.path.join(DIR_PYYEL_PATH, 'Data')
+DIR_DATA_CONFIGS_PATH = os.path.join(DIR_DATA_PATH, 'Configs')
+DIR_DATA_GUIS_PATH = os.path.join(DIR_DATA_PATH, 'GUIs')
+
+# PyYel/Networks
+DIR_NETWORKS_PATH = os.path.join(DIR_PYYEL_PATH, 'Networks')
