8a5c24281c9bcd473a192f0b567dab2bba90522c
Nayel Blidi <nayel.blidi@ipsa.fr>
2024-05-26 12:59:51 +0200

    N/A

--- PyYel/__init__.py
+++ PyYel/__init__.py
@@ -2,11 +2,6 @@
 PyYel packages initializer
 """
 
-from . import Data, Networks
-from .Data import Augmentation, Datapoint, Utils
-from .Networks import Compiler, Models
-
-
 __all__ = ["PyYel",
            "Data", "guis", "configs",
            "Networks",
