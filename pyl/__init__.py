8f4959e2f5754c4265b95106b82675e31e88f868
Nayel-Blidi <nayel.blidi@capgemini.com>
2024-10-07 11:37:41 +0200

    Add: login logout utils

--- pyl/__init__.py
+++ pyl/__init__.py
@@ -2,3 +2,24 @@
 PyYel packages initializer
 """
 
+import os, sys
+
+def login(hf_token: str = None):
+    """
+    PyYel log in helper.
+    Loads the relevant credentials to your environment variables. Those should be removed
+    """
+
+    if hf_token: os.environ["HF_TOKEN"] = hf_token
+
+    return True
+
+def logout():
+    """
+    PyYel log in helper.
+    Loads the relevant credentials to your environment variables. Those should be removed
+    """
+
+    if "HF_TOKEN" in os.environ.keys(): del os.environ["HF_TOKEN"]
+
+    return True
