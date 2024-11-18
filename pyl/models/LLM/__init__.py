a6b3b63d807358133e4660510b260687c1835b85
Nayel Blidi <nayel.blidi@capgemini.com>
2024-11-18 16:58:50 +0100

    Add: renamed models

--- pyl/models/LLM/__init__.py
+++ pyl/models/LLM/__init__.py
@@ -1,9 +1,20 @@
 
-from .src.LLMDecodingMistral7b import LLMDecodingMistral7b
-from .src.LLMDecodingOPT125m import LLMDecodingOPT125m
-from .src.LLMDecodingPhi import LLMDecodingPhi
-from .src.LLMDecodingPhiMoE import LLMDecodingPhiMoE
-from .src.LLMEncodingBARTLargeMNLI import LLMEncodingBARTLargeMNLI
-from .src.LLMEncodingDeBERTaV3Base import LLMEncodingDeBERTaV3Base
-from .src.LLMEncodingDeBERTaV3BaseMNLI import LLMEncodingDeBERTaV3BaseMNLI
-from .src.LLMEncodingDeBERTaV3Large import LLMEncodingDeBERTaV3Large
+__all__ = [
+    "LLMTextGenerationMistral",
+    "LLMTextGenerationOPT",
+    "LLMTextGenerationPhi",
+    "LLMTextSummarizationBART",
+    "LLMTextSummarizationPegasus",
+    "LLMTextSummarizationT5",
+    "LLMZeroShotClassificationBART",
+    "LLMZeroShotClassificationDeBERTaV3",
+]
+
+from .src.LLMTextGenerationMistral import LLMTextGenerationMistral
+from .src.LLMTextGenerationOPT import LLMTextGenerationOPT
+from .src.LLMTextGenerationPhi import LLMTextGenerationPhi
+from .src.LLMTextSummarizationBART import LLMTextSummarizationBART
+from .src.LLMTextSummarizationPegasus import LLMTextSummarizationPegasus
+from .src.LLMTextSummarizationT5 import LLMTextSummarizationT5
+from .src.LLMZeroShootClassificationBART import LLMZeroShotClassificationBART
+from .src.LLMZeroShootClassificationDeBERTaV3 import LLMZeroShotClassificationDeBERTaV3
